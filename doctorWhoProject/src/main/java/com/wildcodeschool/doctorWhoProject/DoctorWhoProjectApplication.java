44d320b44afc12b50c2278131057efccbd69c7e9
agzibuyukselin@gmail.com <agzibuyukselin@gmail.com>
2023-06-20 15:11:48 +0200

    create controller and routes + index html

---
+++ doctorWhoProject/src/main/java/com/wildcodeschool/doctorWhoProject/DoctorWhoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.doctorWhoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorWhoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorWhoProjectApplication.class, args);
+	}
+
+}
---
+++ doctorWhoProject/src/test/java/com/wildcodeschool/doctorWhoProject/DoctorWhoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.doctorWhoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorWhoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
